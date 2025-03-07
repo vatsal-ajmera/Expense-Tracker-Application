54f7ab2e1f766f7ad626acf521b7dc34e28832ba
Vatsal <vatsal@zealousweb.com>
2025-03-07 18:49:26 +0530

    WIP Expense Analysis

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Expense;
+use App\Models\ExpenseCategory;
+use App\Models\Income;
+use Carbon\Carbon;
+use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -21,6 +26,27 @@ class DashboardController extends Controller
     }
     public function index(){
         $auth_user = auth()->guard($this->gaurd);
-        return view('dashboard.index',['meta_data' => $this->meta_data, 'auth_user' => $auth_user]);
+
+        $start_of_month = Carbon::now()->startOfMonth();
+        $end_of_month = Carbon::now()->endOfMonth();
+        $period_income = Income::whereBetween('date', [$start_of_month, $end_of_month])->sum('amount');
+        $period_expenses = Expense::whereBetween('expense_date', [$start_of_month, $end_of_month])->sum('amount');
+
+        $category_expenses = ExpenseCategory::select('expense_categories.category_name', DB::raw('CAST(SUM(expenses.amount) AS DOUBLE) as total'))
+            ->leftJoin('expenses', 'expense_categories.id', '=', 'expenses.category_type_id')
+            ->groupBy('expense_categories.id', 'expense_categories.category_name')
+            ->whereBetween('expenses.expense_date', [$start_of_month, $end_of_month])
+            ->get();
+            
+        $response_data = [
+            'meta_data' => $this->meta_data, 
+            'auth_user' => $auth_user,
+            'category_expenses' => json_encode($category_expenses->pluck('category_name')),
+            'category_total' => json_encode($category_expenses->pluck('total')),
+            'period_income' => (double)formateNumber($period_income),
+            'period_expenses' => (double)formateNumber($period_expenses),
+        ];
+        // dd($response_data);
+        return view('dashboard.index', $response_data);
     }
 }
